da868e82b7eb55de871cdc0e386f512d59afdadd
Mohanraj N.R <mohanrajnr@gmail.com>
2022-01-23 00:17:55 +0530

    Demo User Add App

---
+++ react-user-guide/src/Components/Users/AddUser.js
@@ -0,0 +1,53 @@
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+import { useState } from "react";
+
+const AddUser = (props) => {
+  const [enteredUsername, setUsername] = useState("");
+  const [enteredAge, setAge] = useState("");
+  const clearInput = () => {
+    setUsername("");
+    setAge("");
+  };
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
+    console.log(enteredUsername, enteredAge);
+    clearInput();
+  };
+  const userChangeHandler = (event) => {
+    setUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setAge(event.target.value);
+  };
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          onChange={userChangeHandler}
+          value={enteredUsername}
+          type="text"
+        />
+
+        <label htmlFor="age">Age</label>
+        <input
+          onChange={ageChangeHandler}
+          id="age"
+          value={enteredAge}
+          type="number"
+        />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+export default AddUser;
